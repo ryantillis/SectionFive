799550306b127174adf8aa6d263be02c36b24ff2
Rasa <therain@rasas-air.home>
2019-04-30 17:18:28 -0400

    Switch Demos

--- src/Switch.java
+++ src/Switch.java
@@ -11,20 +11,72 @@ public class Switch {
 //        }
 //    }
 
-    int switchValue = 1;
-    switch(switchValue){
-        case 1:
-            System.out.println("Value was 1");
-            break;
+    public static void main(String[] args) {
 
-        case 2:
-            System.out.println("Value was 2");
-            break;
 
-        default:
-            System.out.println("Was not 1 or 2");
-            break;
+        int switchValue = 6;
+
+        //byte short char int
+
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+
+            case 2:
+                System.out.println("Value was 2");
+                break;
+
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("was a 3, 4, or 5");
+                System.out.println("was" + switchValue);
+                break;
+
+            default:
+                System.out.println("Was not case");
+
+        }
+
+
+        char switchChar = 'A';
+
+        //byte short char int string
+
+        switch (switchChar) {
+            case 'A':
+                System.out.println("A");
+                break;
+
+            case 'B':
+                System.out.println("B");
+                break;
+
+            case 'C':
+                System.out.println("C");
+                break;
+
+            default:
+                System.out.println("Was not case");
+
+        }
+
+        String month = "January";
+
+        //byte short char int
+
+        switch (month) {
+            case "January":
+                System.out.println("Jan");
+                break;
+
+            default:
+                System.out.println("Was not case");
+                break;
+
+        }
+
+
     }
-
-
 }
