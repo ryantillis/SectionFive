d5bd9142eb2e36bdfaffbf08d6da81dbb450cc24
Rasa <therain@Rasas-MacBook-Air.local>
2019-05-02 13:54:19 -0400

    Is35 added

--- src/theForStatement.java
+++ src/theForStatement.java
@@ -13,10 +13,30 @@ public class theForStatement {
         }
         System.out.println(count);
 
+        int sum = 0;
+        int count1 = 0;
+        for(int i=1; i<=1000; i++){
+            if(is35(i)){sum += i; count1++;}else{continue;}
+            if(count1 >= 5){break;}
+        }
+        System.out.println(sum);
+
     }
 
     public static double calculateInterest(double amount, double interestRate) {
         return(amount*(interestRate/100));
+    }
+
+    public static boolean is35(int n) {
+
+        for(int i=3; i <= n; i++){
+            if(n % 3 == 0 && n % 5 == 0){
+                return true;
+            }
+        }
+
+        return false;
+
     }
 
     public static boolean isPrime(int n) {
---
+++ src/theWhileStatement.java
@@ -0,0 +1,8 @@
+public class theWhileStatement {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
